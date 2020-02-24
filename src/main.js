3a791fe77cfc30997a48bc1e3c57720c475eb328
zuoan <pengyingnan0816@163.com>
2020-02-24 15:14:58 +0800

    首页登录

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './style/index.less'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+Vue.use(ElementUI)
 
 new Vue({
   router,
---
+++ src/views/home/index.vue
@@ -0,0 +1,22 @@
+<template>
+<!-- 最外层容器 -->
+  <el-container>
+      <!-- 左侧 -->
+      <el-aside>左侧</el-aside>
+      <!-- 右侧 -->
+      <el-container>
+          <el-header>头部</el-header>
+          <el-main>主体</el-main>
+      </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
